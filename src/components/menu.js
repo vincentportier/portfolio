5dbb0ca8c58ce202a530d7069bb5006f1d883146
Vincent Portier <vinz.portier@gmail.com>
2020-12-07 12:03:28 +1100

    finished hamburger

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,7 +1,79 @@
 import React, { useState } from "react"
 import styled from "styled-components"
 
-const StyledHamburger = styled.div``
+const StyledHamburgerButton = styled.button`
+  display: none;
+
+  @media (max-width: 768px) {
+    ${({ theme }) => theme.mixins.flexCenter};
+    position: relative;
+    z-index: 10;
+    margin-right: -15px;
+    padding: 15px;
+    border: 0;
+    background-color: transparent;
+    text-transform: none;
+    transition-timing-function: linear;
+    transition-duration: 0.15s;
+    transition-property: opacity, filter;
+  }
+
+  .ham-box {
+    display: inline-block;
+    position: relative;
+    width: var(--hamburger-width);
+    height: 24px;
+  }
+
+  .ham-box-inner {
+    position: absolute;
+    top: 50%;
+    right: 0;
+    width: var(--hamburger-width);
+    height: 2px;
+    border-radius: var(--border-radius);
+    background-color: var(--black);
+    transition-duration: 0.22s;
+    transition-property: transform;
+    transition-delay: ${props => (props.menuOpen ? `0.12s` : `0s`)};
+    transform: rotate(${props => (props.menuOpen ? `225deg` : `0deg`)});
+    transition-timing-function: cubic-bezier(
+      ${props =>
+        props.menuOpen ? `0.215, 0.61, 0.355, 1` : `0.55, 0.055, 0.675, 0.19`}
+    );
+    &:before,
+    &:after {
+      content: "";
+      display: block;
+      position: absolute;
+      left: auto;
+      right: 0;
+      width: var(--hamburger-width);
+      height: 2px;
+      border-radius: 4px;
+      background-color: var(--black);
+      transition-timing-function: ease;
+      transition-duration: 0.15s;
+      transition-property: transform;
+    }
+    &:before {
+      width: 100%;
+      top: ${props => (props.menuOpen ? `0` : `-10px`)};
+      opacity: ${props => (props.menuOpen ? 0 : 1)};
+      transition: ${({ menuOpen }) =>
+        menuOpen ? "var(--ham-before-active)" : "var(--ham-before)"};
+    }
+    &:after {
+      width: 100%;
+      bottom: ${props => (props.menuOpen ? `0` : `-10px`)};
+      transform: rotate(${props => (props.menuOpen ? `-90deg` : `0`)});
+      transition: ${({ menuOpen }) =>
+        menuOpen ? "var(--ham-after-active)" : "var(--ham-after)"};
+    }
+  }
+`
+
+const StyledMenu = styled.div``
 
 export const Menu = () => {
   const [menuOpen, setMenuOpen] = useState(false)
@@ -10,16 +82,11 @@ export const Menu = () => {
     setMenuOpen(!menuOpen)
   }
 
-  const activeClass = menuOpen ? "is-active" : null
-
   return (
-    <StyledHamburger
-      className={`hamburger hamburger--elastic ${activeClass}`}
-      onClick={toggleMenu}
-    >
-      <div className="hamburger-box">
-        <div className="hamburger-inner"></div>
+    <StyledHamburgerButton onClick={toggleMenu} menuOpen={menuOpen}>
+      <div className="ham-box">
+        <div className="ham-box-inner" />
       </div>
-    </StyledHamburger>
+    </StyledHamburgerButton>
   )
 }
