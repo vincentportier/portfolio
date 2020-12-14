33381ae4249a27f81bfa8f4c50c40c4f71162a90
Vincent Portier <vinz.portier@gmail.com>
2020-12-14 16:02:28 +1100

    404 and thanks for message pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,70 @@
 import React from "react"
 import Layout from "../components/layout"
+import styled from "styled-components"
+import Helmet from "react-helmet"
+import { Link } from "gatsby"
 
+const StyledContent = styled.main`
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  grid-gap: 10px;
+  align-items: center;
+
+  .error-content {
+    grid-column: 8/12;
+    text-align: center;
+    @media (max-width: 768px) {
+      grid-column: 1/-1;
+    }
+    h1 {
+      color: var(--red);
+      font-family: monospace;
+      font-size: clamp(40px, 10vw, 80px);
+      line-height: 1;
+    }
+    h2 {
+      font-size: clamp(20px, 5vw, 30px);
+      font-weight: 400;
+    }
+  }
+  .error-image {
+    grid-column: 1/8;
+
+    img {
+      display: block;
+      max-width: 400px;
+      margin: 0 auto 0 auto;
+    }
+    @media (max-width: 768px) {
+      display: none;
+    }
+  }
+`
+
+const StyledHomeButton = styled(Link)`
+  ${({ theme }) => theme.mixins.button};
+  &:hover {
+    color: var(--white);
+  }
+  margin-top: 50px;
+  @media (max-width: 768px) {
+    ${({ theme }) => theme.mixins.smallButton}
+  }
+`
 const PageNotFound = () => {
   return (
     <Layout>
-      <div>page not found</div>
+      <Helmet title="Page Not Found" />
+      <StyledContent className="fillHeight">
+        <div className="error-image">
+          <img src="/404.png" alt="404 illustration" />
+        </div>
+        <div className="error-content">
+          <h1>404</h1>
+          <h2>page not found</h2>
+          <StyledHomeButton to="/">Go home</StyledHomeButton>
+        </div>
+      </StyledContent>
     </Layout>
   )
 }
--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,6 +1,36 @@
 import React from "react"
 import Layout from "../components/layout"
+import styled from "styled-components"
+import Helmet from "react-helmet"
+import { Link } from "gatsby"
+
+const StyledContent = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+`
+const StyledHomeButton = styled(Link)`
+  ${({ theme }) => theme.mixins.button}
+  &:hover {
+    color: var(--white);
+  }
+  margin-top: 50px;
+  @media (max-width: 768px) {
+    ${({ theme }) => theme.mixins.smallButton}
+  }
+`
 
 export default function ThankYouPage() {
-  return <Layout>THANK YOU FOR CONTACT ME</Layout>
+  return (
+    <Layout>
+      <Helmet title="Thanks for your message" />
+      <StyledContent className="fillHeight">
+        <h1 className="small-heading">
+          Thank you for your message, I'll get back to you as soon as possible!
+        </h1>
+        <StyledHomeButton to="/">Go back home</StyledHomeButton>
+      </StyledContent>
+    </Layout>
+  )
 }
