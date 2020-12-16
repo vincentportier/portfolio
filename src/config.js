470af7cfb46a9d4aa261428cc793a15e08625e30
Vincent Portier <vinz.portier@gmail.com>
2020-12-16 13:16:11 +1100

    updated projects text

--- src/config.js
+++ src/config.js
@@ -53,10 +53,12 @@ module.exports = {
     {
       title: "vportier.com",
       description:
-        "First iteration of my personal website built with React and hosted on Netlify.",
+        "First iteration of my personal portfolio. I have built it with Gatsby and React and it is hosted with Netlify. Gatsby ships with some great features like a built-in Router or image optimisation tools which convinced me to use it for my website. I used Hooks and Context to manage dark mode and scrollReveal for some of the animations. I plan to learn more about GraphQL to query my website and dynamically render content uploaded in Markdown as the site grows bigger.",
+
       technologies: [
-        "React",
         "Gatsby",
+        "JavaScript",
+        "React",
         "Styled Components",
         "Hooks",
         "Context",
@@ -71,12 +73,12 @@ module.exports = {
         "Toudo is a web app that helps you organize your life. You can create tasks, schedule, prioritise, sort, file and comment on them. Toudo manages authentication and saves your tasks in Firebase. Toudo uses custom hooks to make firebase calls and set the context of the app with your tasks and projects.",
       technologies: [
         "React",
-        "HTML",
-        "CSS",
         "Firebase",
-        "React Router",
         "Hooks",
         "Context",
+        "HTML",
+        "CSS",
+        "React Router",
       ],
       external: "https://toudo.netlify.app",
       github: "https://github.com/vincentportier/toudo",
@@ -85,8 +87,8 @@ module.exports = {
     {
       title: "The Resistance: Avalon",
       description:
-        "This web app is an adaptation of the game The Resistance: Avalon. I used object oriented programming to define the rules of the game and conditional rendering to update the DOM and progress the game based on the players decisions.",
-      technologies: ["React", "HTML", "CSS", "React Router"],
+        "This web app is an adaptation of the game The Resistance: Avalon. The game logic is written in JavaScript. The Javascript is used to update the game parameters based on the number of players, to randomly assign a role to each player, to dynamically update the DOM and to check for end-game conditions.",
+      technologies: ["JavaScript", "React", "React Router", "HTML", "CSS"],
       external: "https://avalon-game.netlify.app",
       github: "https://github.com/vincentportier/avalon-game",
       image: "/avalon.png",
@@ -94,8 +96,8 @@ module.exports = {
     {
       title: "FreeCodeCamp certification projects",
       description:
-        "Each FreeCodeCamp certification requires to complete 5 projects to finalise the course. I have built 15 projects, including static webpages, a random quote generator, a pomodoro clock, a calculator, a markdown text editor and a drum-machine.",
-      technologies: ["HTML", "CSS", "Javascript", "React", "Redux"],
+        "Each FreeCodeCamp certification requires to complete 5 projects to finalise the course. I have built 15 projects, including static webpages, a random quote generator, a pomodoro clock, a calculator, a markdown text editor and a drum-machine. I have centralised my last 5 projects in a GitHub repository where you'll find links to the hosted projects.",
+      technologies: ["JavaScript", "React", "Redux", "HTML", "CSS"],
       github: "https://github.com/vincentportier/FreeCodeCamp",
       image: "/fcc.png",
     },
