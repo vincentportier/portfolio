ca62485f7bc32bcb1ba779a575558662dca76789
Vincent Portier <vinz.portier@gmail.com>
2021-01-11 17:51:41 +1100

    analytics settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,29 +20,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // The property ID; the tracking code won't be generated without it
         trackingId: "G-JVX091GLYL",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: [],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        optimizeId: "G-JVX091GLYL",
-        // Enables Google Optimize Experiment ID
-        experimentId: "G-JVX091GLYL",
-        // Set Variation ID. 0 for original 1,2,3....
-        variationId: "0",
-        // Defers execution of google analytics script after page load
-        defer: false,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
       },
     },
     `gatsby-plugin-react-helmet`,
